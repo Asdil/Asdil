2678944bda1fe70c51c89ca4d5aaf1340621d974
23mofang <23mofang@23mofangdeMacBook-Pro-6.local>
2018-10-09 16:22:58 +0800

    第二版

--- Asdil.py
+++ Asdil.py
@@ -12,6 +12,7 @@
 """
 __author__ = 'Asdil'
 import os
+import shutil
 import subprocess
 
 
@@ -44,4 +45,43 @@ def splitPath(path):
     filePath, tmpFileName = os.path.split(path)
     fileName, extension = os.path.splitext(tmpFileName)
     return filePath, fileName, extension, tmpFileName
+
+
+# 复制文件
+def copyFile(srcfile, dstfile):
+    if not os.path.isfile(srcfile):
+        print("%s not exist!" % srcfile)
+        assert os.path.isfile(srcfile) is True
+    else:
+        fpath, fname = os.path.split(dstfile)    #分离文件名和路径
+        if not os.path.exists(fpath):
+            os.makedirs(fpath)                 #创建路径
+        shutil.copyfile(srcfile, dstfile)      #复制文件
+        print("copy %s -> %s" % (srcfile, dstfile))
+
+
+# 剪切文件
+def cutFile(srcfile,dstfile):
+    if not os.path.isfile(srcfile):
+        print("%s not exist!" % srcfile)
+        assert os.path.isfile(srcfile) is True
+    else:
+        fpath, fname = os.path.split(dstfile)    # 分离文件名和路径
+        if not os.path.exists(fpath):
+            os.makedirs(fpath)                 # 创建路径
+        shutil.move(srcfile, dstfile)          # 复制文件
+        print("copy %s -> %s" % (srcfile, dstfile))
+
+
+# 帮助文档
+def help():
+    print("函数: pathJoin(path1, path2)合并文件")
+    print("函数: subprocessPopen(cmd)执行shell命令")
+    print("函数: subprocessCall(cmd)执行shell命令获取返回值")
+    print("函数: splitPath(path)分离目录和文件")
+    print("函数: copyFile(srcfile, dstfile)复制文件")
+    print("函数: cutFile(srcfile,dstfile)剪切文件")
+
+
+
 
