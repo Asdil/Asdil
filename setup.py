33d33161346b5271eec138446843f34898e6e9dc
23mofang <23mofang@23mofangdeMacBook-Pro-8.local>
2018-12-28 17:35:23 +0800

    修复时间2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@
 -------------------------------------------------
    Change Activity:
                    2018/10/26:
-    version = 1.7.1.2
+    version = 1.7.2.3
 -------------------------------------------------
 """
 __author__ = 'Asdil'
@@ -17,7 +17,7 @@ __author__ = 'Asdil'
 from setuptools import setup
 
 setup(name='Asdil',
-      version='1.7.1.2',
+      version='1.7.2.3',
       description='Tool of Asdil',
       author='Asdil',
       author_email='jpl4job@126.com',
